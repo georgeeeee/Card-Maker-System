1a1d51381248f4ea068d711518839d28787ab677
imaslarab <baralsami@gmail.com>
2019-11-01 16:05:00 -0400

    Add http responses for Create card, delete card and list cards response

---
+++ src/main/java/edu/wpi/cs/justice/cardmaker/http/DeleteCardRequest.java
@@ -0,0 +1,10 @@
+package edu.wpi.cs.justice.cardmaker.http;
+
+public class DeleteCardRequest {
+	public final int cardId;
+	
+	public DeleteCardRequest (int cardId) {
+		this.cardId = cardId;
+	}
+
+}
---
+++ src/main/java/edu/wpi/cs/justice/cardmaker/http/ListCardsResponse.java
@@ -0,0 +1,25 @@
+package edu.wpi.cs.justice.cardmaker.http;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.cs.justice.cardmaker.model.Card;
+
+public class ListCardsResponse {
+	public final List<Card> cardsList;
+	public final int statusCode;
+	public final String error;
+	
+	public ListCardsResponse (List<Card> cards, int code) {
+		this.cardsList = cards;
+		this.statusCode = code;
+		this.error = "";
+	}
+	
+	public ListCardsResponse (int code, String errorMessage) {
+		this.cardsList = new ArrayList<Card>();
+		this.statusCode = code;
+		this.error = errorMessage;
+	}
+}
+
