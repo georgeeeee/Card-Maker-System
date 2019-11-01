6f908974a32ec3f30a3d4292d591093562283087
imaslarab <baralsami@gmail.com>
2019-11-01 16:05:24 -0400

    Add card model

--- src/main/java/edu/wpi/cs/justice/cardmaker/model/Card.java
+++ src/main/java/edu/wpi/cs/justice/cardmaker/model/Card.java
@@ -1,20 +1,15 @@
 package edu.wpi.cs.justice.cardmaker.model;
 
 public class Card {
-	int cardId;
+	final String cardId;
 	String eventType;
 	String recipient;
 	String orientation;
 	
-	public Card(String eventType, String recipient, String orientation) {
-		this.cardId = randomizer();
+	public Card(String cardId, String eventType, String recipient, String orientation) {
+		this.cardId = cardId;
 		this.eventType = eventType;
 		this.recipient = recipient;
 		this.orientation = orientation;
-	}
-
-	private int randomizer() {
-		
-		return 0;
 	}
 }
