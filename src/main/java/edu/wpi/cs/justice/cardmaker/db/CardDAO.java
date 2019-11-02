16428617d5a40854fe1a6af809e72af6af703210
imaslarab <baralsami@gmail.com>
2019-11-01 20:33:06 -0400

    Add methods to delete and list cards from db

--- src/main/java/edu/wpi/cs/justice/cardmaker/db/CardDAO.java
+++ src/main/java/edu/wpi/cs/justice/cardmaker/db/CardDAO.java
@@ -1,6 +1,9 @@
 package edu.wpi.cs.justice.cardmaker.db;
 
-import java.sql.PreparedStatement;
+import java.sql.*;
+import java.util.ArrayList;
+
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 import edu.wpi.cs.justice.cardmaker.model.Card;
 
@@ -8,11 +11,16 @@ import edu.wpi.cs.justice.cardmaker.model.Card;
 public class CardDAO {
 	java.sql.Connection conn;
 
+	LambdaLogger logger;
+	
     public CardDAO() {
+    	if (logger != null) { logger.log("in db creation"); }
+    	
     	try  {
     		conn = DatabaseUtil.connect();
     	} catch (Exception e) {
     		conn = null;
+    		if (logger != null) { logger.log(e.getMessage()); }
     	}
     }
     
@@ -29,6 +37,49 @@ public class CardDAO {
         } catch (Exception e) {
             throw new Exception("Failed to add card: " + e.getMessage());
         }
+    }
+
+    public boolean deleteCard(String cardId) throws Exception {
+        try {
+            PreparedStatement ps = conn.prepareStatement("DELETE FROM constants WHERE card_id = ?;");
+            ps.setString(1, cardId);
+            int numAffected = ps.executeUpdate();
+            ps.close();
+            
+            return (numAffected == 1);
+
+        } catch (Exception e) {
+            throw new Exception("Failed to delete: " + e.getMessage());
+        }
+    }
+    
+    public ArrayList<Card> getAllCards() throws Exception {
+        
+        ArrayList<Card> cards = new ArrayList<Card>();
+        try {
+            Statement statement = conn.createStatement();
+            String query = "SELECT * FROM cards";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                Card c = generateCard(resultSet);
+                cards.add(c);
+            }
+            resultSet.close();
+            statement.close();
+            return cards;
+
+        } catch (Exception e) {
+            throw new Exception("Failed to get cards: " + e.getMessage());
+        }
+    }
+    
+    private Card generateCard(ResultSet resultSet) throws Exception {
+    	String cardId  = resultSet.getString("card_id");
+        String eventType  = resultSet.getString("event_type");
+        String recipient  = resultSet.getString("recipient");
+        String orientation  = resultSet.getString("orientation");
+        return new Card (cardId, eventType, recipient, orientation);
     }
 
 }
